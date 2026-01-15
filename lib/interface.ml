30943cdd7db806eb6dc3f4c53a31e36548ca52e1
Jennifer Paykin <jpaykin@gmail.com>
2026-01-15 14:46:49 +0100

    Made it easier to change dimensions in interface

--- lib/interface.ml
+++ lib/interface.ml
@@ -1,9 +1,21 @@
 (* top-level interface for interacting with LambdaPC via the interpreter or other *)
 
 include LambdaPC.HOAS
+
 module S2 = Scalars.Scalars (Scalars.FIN2)
 module Eval2 = LambdaPC.Eval(S2)
 module Typing2 = Typing.SmtLambdaPC(S2)
+
+module S3 = Scalars.Scalars (Scalars.FIN3)
+module Eval3 = LambdaPC.Eval(S3)
+module Typing3 = Typing.SmtLambdaPC(S3)
+
+module S4 = Scalars.Scalars (Scalars.FIN4)
+module Eval4 = LambdaPC.Eval(S4)
+module Typing4 = Typing.SmtLambdaPC(S4)
+
+let dim = ref 2
+let set_dimension d = dim := d
 
 let parse (s : string) : LambdaPC.Expr.t =
   let lexbuf = Lexing.from_string s in
@@ -22,9 +34,21 @@ let parseFromFile (filename : string) : LambdaPC.Expr.t =
 
 let eval e =
   print_endline (LambdaPC.Expr.pretty_string_of_t e ^ "\n->*\n");
-  let result = Eval2.evalClosed e in
+  let eval_closed = match !dim with
+             | 2 -> Eval2.evalClosed
+             | 3 -> Eval3.evalClosed
+             | 4 -> Eval4.evalClosed
+             | d -> failwith @@ "Please add evaluation module for dimension " ^ string_of_int d ^ "\n"
+  in
+  let result = eval_closed e in
   print_endline (LambdaPC.Val.string_of_t result ^ "\n")
 
 let typecheck pc =
-  let (tp1,tp2) = Typing2.typecheck pc in
+  let typecheck_d = match !dim with
+             | 2 -> Typing2.typecheck
+             | 3 -> Typing3.typecheck
+             | 4 -> Typing4.typecheck
+             | d -> failwith @@ "Please add typing module to interface.ml for dimension " ^ string_of_int d ^ "\n"
+  in
+  let (tp1,tp2) = typecheck_d pc in
   print_endline @@ LambdaPC.Expr.pretty_string_of_pc pc ^ " : |" ^ LambdaPC.Type.string_of_t tp1 ^ " -o " ^ LambdaPC.Type.string_of_t tp2 ^"|"
